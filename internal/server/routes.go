6df9c6c62cb6aa909eb97999a850b8ee4c526a62
Bhaskar <bhaskarmandal369@gmail.com>
2024-12-02 15:26:40 +0530

    refactor: server config

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,21 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (s *Server) RegisterRoutes() http.Handler {
-	r := gin.Default()
+func (s *Server) RegisterRoutes() {
+	// r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
+	s.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"}, // Add your frontend URL
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true, // Enable cookies/auth
 	}))
 
-	r.GET("/", s.HelloWorldHandler)
+	s.GET("/", s.HelloWorldHandler)
+	s.GET("/health", s.healthHandler)
 
-	r.GET("/health", s.healthHandler)
-
-	return r
+	// return r
 }
 
 func (s *Server) HelloWorldHandler(c *gin.Context) {
